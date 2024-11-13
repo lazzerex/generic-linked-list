2c654096ff769e473a02d30bc0ad8b9a3cc57bf9
lazzerex <150422206+lazzerex@users.noreply.github.com>
2024-11-13 16:09:24 +0700

    Add files via upload

---
+++ generic-linked-list/generic-linked-list/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic_linked_list
+{
+    internal class Program
+    {
+
+        static void print_Reverse(LinkedList<string> list)
+        {
+            Console.WriteLine("---In reverse order---");
+            LinkedListNode<string> current = list.Last;
+            while (current != null)
+            {
+                Console.WriteLine(current.Value);
+                current = current.Previous;
+            }
+
+            Console.WriteLine("--------------");
+        }
+        static void Main(string[] args)
+        {
+            LinkedList<string> names = new LinkedList<string>();
+
+            LinkedListNode<string> node = new LinkedListNode<string>("Arc");
+            names.AddFirst(node);
+
+            LinkedListNode<string> node1 = new LinkedListNode<string>("Gamma");
+            names.AddAfter(node, node1);
+
+            LinkedListNode<string> node2 = new LinkedListNode<string>("Geed");
+            names.AddAfter(node1, node2);
+
+            LinkedListNode<string> node3 = new LinkedListNode<string>("Decker");
+            names.AddAfter(node2, node3);
+
+            
+            LinkedListNode<string> node4 = new LinkedListNode<string>(null);
+
+            Console.WriteLine("List: ");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Numbers of element in list: " + names.Count);
+            Console.WriteLine("First element: " + names.First.Value);
+            Console.WriteLine("Last element: " + names.Last.Value);
+
+            names.AddLast("Ginga");
+
+            names.AddFirst("Orb");
+
+            names.AddBefore(node3, "Jack");
+
+            Console.WriteLine("\nList after added new elements:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.Remove(node2);
+            Console.WriteLine("\nList after deleting node2 (Geed):");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nList contains 'Arc': " + names.Contains("Arc"));
+
+            print_Reverse(names);
+
+            Console.ReadLine();
+
+        }
+    }
+}
